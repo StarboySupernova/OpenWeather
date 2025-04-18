d0ceccc6fcf80f82e052433a511c1a28217b8ed3
Christian Simbarashe <supernovaonline@outlook.com>
2025-04-18 17:08:28 +0100

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by user276583 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
